5e43b09ef7a17887d94d3924a57d2d662c3298a1
Luis Martinez <luizmartiinez@gmail.com>
2018-05-04 19:18:49 -0400

    Popular 120 tweets

--- src/main/scala/SentimentAnalysis.scala
+++ src/main/scala/SentimentAnalysis.scala
@@ -16,7 +16,7 @@ object SentimentAnalysis {
     var consumerSecret = "RyCgiLKqP8kQjcwxJ8h9EQFzLGm3dL5n2eCTN9YpQ2RRYG3cd7"
     var accessToken = "931749427211128832-UJP8jUVAEieK0fP9mmHn5yuD4DiGi8M"
     var accessTokenSecret = "TAuHxwpL6FghEom8IDUtQTQPUeHik6nxhjmhzvyGlfGUk"
-    var filters = Seq("Trump")
+    //var filters = Seq("Trump")
 
   if (args.length > 3) {
       // get data from your setting
@@ -37,37 +37,50 @@ object SentimentAnalysis {
     val ssc = new StreamingContext(sparkConf, Seconds(5))
     val stream = TwitterUtils.createStream(ssc, None, filters)
 
-    val tags = stream.flatMap { status => status.getHashtagEntities.map(_.getText)}
+    //val tags = stream.flatMap { status => status.getHashtagEntities.map(_.getText)}
+      
+    val alltweets = stream.flatMap(status => status.getText.split(" "))
 
-    // save tages to files
-    tags.countByValue()
-      .foreachRDD { rdd =>val now = org.joda.time.DateTime.now()
-        val savepath = now.toString("yyyy_MM_dd__hh__mm")
-        rdd.sortBy(_._2).map(x => (x, now)).saveAsTextFile(s"output/tags/$savepath")
-      }
+val topCounts120 = alltweets.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(120)).map{case (topic, count) => (count, topic)}.transform(_.sortByKey(false))
+val topCounts30 = alltweets.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(30)).map{case (topic, count) => (count, topic)}.transform(_.sortByKey(false))
 
 
-    // save whole live twitter in trump data
-    val tweets = stream.filter {t =>
-      val tags = t.getText.split(" ").filter(_.startsWith(filters(0))).map(_.toLowerCase)
-      tags.exists { x => true }
-    }
+//    // save tages to files
+//    tags.countByValue()
+//      .foreachRDD { rdd =>val now = org.joda.time.DateTime.now()
+//        val savepath = now.toString("yyyy_MM_dd__hh__mm")
+//        rdd.sortBy(_._2).map(x => (x, now)).saveAsTextFile(s"output/tags/$savepath")
+//      }
+      
+    // Print popular hashtags
+topCounts120.foreachRDD(rdd => {
+  val topList = rdd.take(10)
+  println("\nPopular topics in last 120 seconds (%s total):".format(rdd.count()))
+  topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
+})
 
-
-    val data = tweets.map { status =>
-      val sentiment = SentimentAnalysisUtils.detectSentiment(status.getText)
-
-      val tagss = status.getHashtagEntities.map(_.getText.toLowerCase)
-        println(status.getText,  tagss.toString(),sentiment.toString)
-      println("=======================================================")
-      (status.getText,  tagss.toString(),sentiment.toString)
-
-
-    }
-
-    // save path
-
-    data.saveAsTextFiles("output/twitter_and_rating/t")
+//
+//    // save whole live twitter in trump data
+//    val tweets = stream.filter {t =>
+//      val tags = t.getText.split(" ").filter(_.startsWith(filters(0))).map(_.toLowerCase)
+//      tags.exists { x => true }
+//    }
+//
+//
+//    val data = tweets.map { status =>
+//      val sentiment = SentimentAnalysisUtils.detectSentiment(status.getText)
+//
+//      val tagss = status.getHashtagEntities.map(_.getText.toLowerCase)
+//        println(status.getText,  tagss.toString(),sentiment.toString)
+//      println("=======================================================")
+//      (status.getText,  tagss.toString(),sentiment.toString)
+//
+//
+//    }
+//
+//    // save path
+//
+//    data.saveAsTextFiles("output/twitter_and_rating/t")
 
     ssc.start()
     ssc.awaitTermination()
