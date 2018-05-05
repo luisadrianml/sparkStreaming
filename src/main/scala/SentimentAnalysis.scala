9b1eb0f4e447af3597ce51e35ca7c96a799736f3
Luis Martinez <luizmartiinez@gmail.com>
2018-05-05 16:25:09 -0400

    Fixed some issues

--- src/main/scala/SentimentAnalysis.scala
+++ src/main/scala/SentimentAnalysis.scala
@@ -12,18 +12,20 @@ object SentimentAnalysis {
 
     // Get Twitter Token and the filter words
     // By default, use my token and filter word is Trump
-    var consumerKey ="PD7ERIVkqJ3xsg1V0rwmu43ok"
-    var consumerSecret = "RyCgiLKqP8kQjcwxJ8h9EQFzLGm3dL5n2eCTN9YpQ2RRYG3cd7"
-    var accessToken = "931749427211128832-UJP8jUVAEieK0fP9mmHn5yuD4DiGi8M"
-    var accessTokenSecret = "TAuHxwpL6FghEom8IDUtQTQPUeHik6nxhjmhzvyGlfGUk"
-//    var filters = Seq("Trump")
+    var consumerKey ="SweK0ImF4kqSk26cEHZ2t4C47"
+    var consumerSecret = "eA5vufQY7Kr24wxziRLQV8LVTjY61X1J0REA90ULfkGOaE1ema"
+    var accessToken = "79481024-t0FHjhajmtNZPZoatkfX8CuUOBCEgD8UDL5pzvRN7"
+    var accessTokenSecret = "LX9NSy1MzPLl1HvAEztvhBaQvSo46TqXdpvygjIyN4jbT"
+    var time = 0;
 
   if (args.length > 3) {
       // get data from your setting
     val  Array(consumerKey, consumerSecret, accessToken, accessTokenSecret) = args.take(4)
-//      filters = args.takeRight(args.length - 4)
+      time = toInt(args.takeRight(args.length - 4))
+      if (time==0) {
+          time = 120;
+      }
     }
-
 
     // Set the system properties so that Twitter4j library used by twitter stream
     // can use them to generat OAuth credentials
@@ -31,53 +33,39 @@ object SentimentAnalysis {
     System.setProperty("twitter4j.oauth.consumerSecret", consumerSecret)
     System.setProperty("twitter4j.oauth.accessToken", accessToken)
     System.setProperty("twitter4j.oauth.accessTokenSecret", accessTokenSecret)
+      
+    print(consumeKey)
 
     // Set twitter stream
     val sparkConf = new SparkConf().setAppName("Twitter").setMaster("local[2]")
     val ssc = new StreamingContext(sparkConf, Seconds(5))
     val stream = TwitterUtils.createStream(ssc, None)      //filters was inside the stream
 
-
     val tags = stream.flatMap { status => status.getHashtagEntities.map(_.getText)}
       
-    val topCount = tags.map((_, 1)).reduceByKeyAndWindow((a:Int,b:Int) => (a + b), Seconds(300),Seconds(60)).map{case (topic, count) => (count, topic)}.transform(_.sortByKey(false))
+    val topCount = tags.map((_, 1)).reduceByKeyAndWindow((a:Int,b:Int) => (a + b), Seconds(time),Seconds(time)).map{case (topic, count) => (count, topic)}.transform(_.sortByKey(false))
       
     // Print popular hashtags
-topCount.foreachRDD(rdd => {
-  val topList = rdd.take(10)
-  println("\nPopular topics in last 300 seconds every 60 minutes (%s total):".format(rdd.count()))
-  topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
-})
+    topCount.foreachRDD(rdd => {
+    val topList = rdd.take(10)
+    println(s"\nPopular topics in last $time seconds (%s total):".format(rdd.count()))
+    topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
+    })
       
-      topCount.saveAsTextFiles("output/populars/tags")
-
-//
-//    // save whole live twitter in trump data
-//    val tweets = stream.filter {t =>
-//      val tags = t.getText.split(" ").filter(_.startsWith(filters(0))).map(_.toLowerCase)
-//      tags.exists { x => true }
-//    }
-//
-//
-//    val data = tweets.map { status =>
-//      val sentiment = SentimentAnalysisUtils.detectSentiment(status.getText)
-//
-//      val tagss = status.getHashtagEntities.map(_.getText.toLowerCase)
-//        println(status.getText,  tagss.toString(),sentiment.toString)
-//      println("=======================================================")
-//      (status.getText,  tagss.toString(),sentiment.toString)
-//
-//
-//    }
-//
-//    // save path
-//
-//    data.saveAsTextFiles("output/twitter_and_rating/t")
+    topCount.saveAsTextFiles("output/populars/tags")
 
     ssc.start()
     ssc.awaitTermination()
 
   }
+    
+    def toInt(s: String): Int = {
+      try {
+        s.toInt
+      } catch {
+        case e: Exception => 0
+      }
+}
 
 }
 
